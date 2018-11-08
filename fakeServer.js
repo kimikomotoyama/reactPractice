9e4f6bafb882d03c5bcc369f996cb81dad60c0e3
Glitch (hello-webpage) <none>
2018-11-08 09:42:57 +0000

    👰📻 Checkpoint ./fakeServer.js:2106920/2165 ./index.html:2106920/666 ./script.js:2106920/187 ./style.css:2106920/51

--- fakeServer.js
+++ fakeServer.js
@@ -1,5 +1,42 @@
 class Server {
-    hello() {
-      console.log("Hi");
-    }
+
+  isIdValid(id, callback) {
+    setTimeout(() => {
+      if (id !== 'bob') {
+        callback(true);
+      } else {
+        callback(false);
+      }
+    }, 100);
+  }
+  
+  loadCountries(callback) {
+    setTimeout(() => {
+      callback([
+        {id: 'JP', label: 'Japan'},
+        {id: 'FR', label: 'France'},
+        {id: 'USA', label: 'USA'}
+      ]);
+    }, 100);
+  }
+
+  register(user, callback) {
+    setTimeout(() => {
+      try {
+        if (!user.id) {
+          callback({error: "Missing id"});
+        } else if (!user.name) {
+          callback({error: "Missing name"});
+        } else if (!user.email) {
+          callback({error: "Missing e-mail"});
+        }
+        callback({
+          // TODO
+        });
+      } catch (e) {
+        console.error("Boom", e);
+        callback({error: "Unexpected error"});
+      }
+    }, 100);
+  }
 }
--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-new Server().hello();
+const server = new Server();
+
+server.register(null, (something) => {
+  console.log("Response: ", something);
+});
