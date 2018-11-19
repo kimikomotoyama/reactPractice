a6b91d97cca4329ed0eaeb2a8767d259ab9450f6
Glitch (orchid-kilogram) <none>
2018-11-19 06:22:15 +0000

    🖼🌁 Checkpoint ./index.html:2106920/1486 ./index.jsx:2106920/487 ./fakeServer.js:2106920/14

--- index.jsx
+++ index.jsx
@@ -1,4 +1,12 @@
-/* global ReactDOM */
+/* global server, ReactDOM */
+
+// TODO : implement form, validation, etc
+
+server.register(null, (something) => {
+  console.log("Response: ", something);
+});
+
+// React
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('root')
--- vanilla.js
+++
@@ -1,5 +0,0 @@
-// TODO : implement form, validation, etc
-
-window.server.register(null, (something) => {
-  console.log("Response: ", something);
-});
