740e21d5a29be64926651ca3797243c8e2a909e4
Glitch (orchid-kilogram) <none>
2018-11-21 05:04:51 +0000

    ☺️👢 Checkpoint ./index.jsx:2338767/2793

--- index.jsx
+++ index.jsx
@@ -9,22 +9,75 @@ server.register(badUser, (result, error) => {
   }
 });
 
-class Form extends React.Component {
+class Button extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>{this.props.name}</button>
+      </div>
+    );
+  }
+}
 
+class Dropdown extends React.Component {
+  render() {
+    return (
+      <div>
+        <select name={this.props.name}>
+          <option value="test">Test</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+class Label extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.label}
+      </div>
+    );
+  }
+}
+
+class TextField extends React.Component {
+  render() {
+    return (
+      <div>
+        <Label label={this.props.label}/>
+        <input></input>
+      </div>
+    );
+  }
+}
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <TextField label="User Id"/>
+        <TextField label="Name"/>
+        <TextField label="E-mail"/>
+        <Dropdown name="countries"/>
+        <Button name="Submit"/>
+      </div>
+    );
+  }
 }
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        Home
+        <Form />
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <Home></Home>,
+  <Home />,
   document.getElementById('root')
 );
 
