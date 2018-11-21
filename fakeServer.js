7f0dcd4803b2be47a8dd125d56f122c4015c45cf
Glitch (orchid-kilogram) <none>
2018-11-21 15:52:25 +0000

    🦄🏫 Checkpoint ./fakeServer.js:2338767/27 ./index.jsx:2338767/203

--- fakeServer.js
+++ fakeServer.js
@@ -3,7 +3,7 @@ class Server {
   isIdValid(id, callback) {
     setTimeout(() => {
       if (id !== 'bob') {
-        callback(200);
+        callback(true);
       } else {
         callback(false);
       }
