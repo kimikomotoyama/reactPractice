867e35d2bcc2540d69ade7de82ae8ba0de0016ba
Glitch (orchid-kilogram) <none>
2018-11-29 09:04:52 +0000

    🏢👲 Checkpoint ./form.jsx:2338767/192 ./formControlComp.jsx:2338767/1042

--- form.jsx
+++ form.jsx
@@ -30,42 +30,42 @@ class Form extends React.Component {
     });
   }
   
-  verify(input, type, callback) {
-    if (type === 'id') {
-      server.isIdValid(input, (response) => {
-        callback(response);
-      });
-    } else if (type === 'name') {
-      if (input.length > 2) {
-        callback(true);
-      } else {
-        callback(false);
-      }
-    } else if (type === 'email') {
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
-        callback(true);
-      } else {
-        callback(false);
-      }
-    }
-  }
+//   verify(input, type, callback) {
+//     if (type === 'id') {
+//       server.isIdValid(input, (response) => {
+//         callback(response);
+//       });
+//     } else if (type === 'name') {
+//       if (input.length > 2) {
+//         callback(true);
+//       } else {
+//         callback(false);
+//       }
+//     } else if (type === 'email') {
+//       if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+//         callback(true);
+//       } else {
+//         callback(false);
+//       }
+//     }
+//   }
   
-  verifyEachInput(type, value, verifyKey) {
-    this.verify(value, type, (verified) => {
-        const verifyObj = {};
-        const userInput = {
-          ...this.state.userInputs
-        };
-        userInput[type] = value;
+//   verifyEachInput(type, value, verifyKey) {
+//     this.verify(value, type, (verified) => {
+//         const verifyObj = {};
+//         const userInput = {
+//           ...this.state.userInputs
+//         };
+//         userInput[type] = value;
           
-        this.setState({userInputs: userInput}, () => {
-          console.log(this.state.userInputs);
-          verifyObj[verifyKey] = verified ? "success": "error";
-          console.log(verifyObj + ": " + verifyObj[verifyKey]);
-          this.setState(verifyObj, () => this.allowDisableSubmitBtn());
-        });
-      });
-  }
+//         this.setState({userInputs: userInput}, () => {
+//           console.log(this.state.userInputs);
+//           verifyObj[verifyKey] = verified ? "success": "error";
+//           console.log(verifyObj + ": " + verifyObj[verifyKey]);
+//           this.setState(verifyObj, () => this.allowDisableSubmitBtn());
+//         });
+//       });
+//   }
   
   handleInput(e, type) { 
     let verified;
--- formControlComp.jsx
+++ formControlComp.jsx
@@ -1,6 +1,44 @@
 /* global server, window, React, ReactDOM, ReactBootstrap Button Dropdown Form PropTypes */
 
 class FormControlComp extends React.Component {
+  
+  verify(input, type, callback) {
+    if (type === 'id') {
+      server.isIdValid(input, (response) => {
+        callback(response);
+      });
+    } else if (type === 'name') {
+      if (input.length > 2) {
+        callback(true);
+      } else {
+        callback(false);
+      }
+    } else if (type === 'email') {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+        callback(true);
+      } else {
+        callback(false);
+      }
+    }
+  }
+  
+  verifyEachInput(type, value, verifyKey) {
+    this.verify(value, type, (verified) => {
+        const verifyObj = {};
+        const userInput = {
+          ...this.state.userInputs
+        };
+        userInput[type] = value;
+          
+        this.setState({userInputs: userInput}, () => {
+          console.log(this.state.userInputs);
+          verifyObj[verifyKey] = verified ? "success": "error";
+          console.log(verifyObj + ": " + verifyObj[verifyKey]);
+          this.setState(verifyObj, () => this.allowDisableSubmitBtn());
+        });
+      });
+  }
+  
   render() {
     return (
       <FormControl type={this.props.type} onChange={this.props.onChange}/>
